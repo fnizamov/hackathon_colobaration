8ffbdd00471650bbd777aee1fbf98219ffbda2c7
Tsunoyaka <mousakun@gmail.com>
2022-11-22 19:55:56 +0600

    fix/account_update

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Genre, Book, BookImage
 
-# admin.site.register((Tag, Comment))
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug']
